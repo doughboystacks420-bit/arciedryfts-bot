bfdfaf5ec94cee25e275cee9ce31823de5cc471e
doughboystacks420-bit <doughboystacks420@gmail.com>
2026-06-10 21:01:54 -0400

    Fix activity name formatting in bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "diffently DAB time:honey_pot: ",
+        name: "diffently DAB time",
         // Activity type number (0 = Playing).
         type: 0, 
       },
