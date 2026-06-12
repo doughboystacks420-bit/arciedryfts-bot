638072512acca391c0fbc6ed0412b2c6b65ef45d
doughboystacks420-bit <doughboystacks420@gmail.com>
2026-06-11 20:54:50 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "diffently DAB time",
+        name: "Difinitely DAB time🍯",
         // Activity type number (0 = Playing).
         type: 0, 
       },
